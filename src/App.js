7989553d6e67d5a41fbd2232f9f1397da04e3f35
Nurzhan Bizhan <bizhannk@gmail.com>
2023-10-05 19:25:23 +0600

    update logic ans styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
-import "./App.css";
-import CustomWebcam from "./components/CustomWebCam";
-
+import { useState } from 'react';
+import './App.css';
+import CustomWebcam from './components/CustomWebCam';
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-  return <div className="App">
-    {!isOpen &&
-    <button onClick={()=> setIsOpen(true)}>Take a photo</button> }
-    {isOpen && 
-     <CustomWebcam />
-    }
-  </div>;
+  return (
+    <div className="App">
+      {!isOpen && (
+        <div className="btn-container">
+          <button onClick={() => setIsOpen(true)}>Take a photo</button>
+        </div>
+      )}
+      {isOpen && <CustomWebcam />}
+    </div>
+  );
 };
 
 export default App;
--- src/components/CustomWebCam.jsx
+++ src/components/CustomWebCam.jsx
@@ -1,46 +1,113 @@
-import { useState } from "react";
-import { useCallback } from "react";
-import { useRef } from "react";
-import Webcam from "react-webcam";
-import CameraIcon from "./CameraIcon";
+import { useEffect, useState } from 'react';
+import { useCallback } from 'react';
+import { useRef } from 'react';
+import Webcam from 'react-webcam';
+import CameraIcon from './CameraIcon';
 
 const CustomWebcam = () => {
   const [mirrored, setMirrored] = useState(false);
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
+  const [watermarkedImgSrc, setWatermarkedImgSrc] = useState(null);
+
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
+
+    // Call a function to add watermark when capturing the image
+    addWatermark(imageSrc);
   }, [webcamRef]);
+
   const retake = () => {
     setImgSrc(null);
+    setWatermarkedImgSrc(null);
   };
-  const videoConstraints = {
-      facingMode: { exact: "environment" }
+
+  const addWatermark = (imageSrc) => {
+    // Create a new Image object
+    const image = new Image();
+    image.src = imageSrc;
+
+    // When the image is loaded, draw it on a canvas with the watermark
+    image.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const ctx = canvas.getContext('2d');
+
+      // Draw the image on the canvas
+      ctx.drawImage(image, 0, 0);
+
+      // Add a watermark text
+      ctx.font = '20px Arial';
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+      ctx.fillText('Your Watermark Here', 10, canvas.height - 10);
+
+      // Convert the canvas to a data URL and set it as the watermarked image source
+      const watermarkedImageSrc = canvas.toDataURL('image/jpeg');
+      setWatermarkedImgSrc(watermarkedImageSrc);
     };
+  };
+
+  const videoConstraints = {
+    facingMode: { exact: 'environment' },
+  };
+
+  const sendToServer = () => {
+    if (watermarkedImgSrc) {
+      // Create a FormData object to send the image
+      const formData = new FormData();
+      formData.append('image', watermarkedImgSrc);
+      console.log(formData);
+      // Make an HTTP POST request to your server
+      // fetch('https://example.com/upload', {
+      //   method: 'POST',
+      //   body: formData,
+      // })
+      //   .then((response) => {
+      //     // Handle the server response as needed
+      //     if (response.ok) {
+      //       // Image was successfully uploaded
+      //       console.log('Image uploaded successfully.');
+      //     } else {
+      //       // Handle any errors
+      //       console.error('Image upload failed.');
+      //     }
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error sending the image:', error);
+      //   });
+    }
+  };
+
   return (
     <div className="container">
-    {imgSrc ? (
-      <img src={imgSrc} alt="webcam" />
-    ) : (
-      <Webcam
-          height={400}
+      {watermarkedImgSrc ? (
+        <div className="imageContainer">
+          <img src={watermarkedImgSrc} alt="webcam" />
+        </div>
+      ) : (
+        <Webcam
+          height={300}
           width={300}
           ref={webcamRef}
           mirrored={mirrored}
           screenshotFormat="image/jpeg"
           screenshotQuality={1}
-          videoConstraints={videoConstraints}
+          // videoConstraints={videoConstraints}
         />
-    )}
-    <div className="btn-container">
-      {imgSrc ? (
-        <button onClick={retake}>Переснять</button>
-      ) : (
-        <button onClick={capture}>Снять фото</button>
       )}
+      <div className="btn-container">
+        {watermarkedImgSrc ? (
+          <>
+            <button onClick={retake}>Переснять</button>
+            <button onClick={sendToServer}>Отправить</button>
+          </>
+        ) : (
+          <button onClick={capture}>Снять фото</button>
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 
