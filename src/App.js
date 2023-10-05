5b3bc77e1531f261070571780c2b5533b1e199ce
Nurzhan Bizhan <bizhannk@gmail.com>
2023-10-05 18:54:31 +0600

    add state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import CustomWebcam from "./components/CustomWebCam";
 
+
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return <div className="App">
+    <button onClick={()=> setIsOpen(true)}>Take a photo</button>
+    {isOpen && 
      <CustomWebcam />
+    }
   </div>;
 };
 
